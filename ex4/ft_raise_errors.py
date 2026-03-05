9a573600c56f99c3874ddbbedca51586487ff90d
Marouane Dahhou <mdahhou@e3r1p11.1337.ma>
2026-03-05 16:28:41 +0000

    s

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -1,57 +1,52 @@
+#!/usr/bin/env python3
 def check_plant_health(plant_name: str, water_level: int, sunlight_hours: int) -> None:
-    try:
-        if plant_name == "" or None:
-            raise ValueError(f"Error: Plant name cannot be empty!")
-        elif plant_name == None:
-            print("Error: Plant name cannot be None!")
-        else:
-            print(f"Plant '{plant_name}' is healthy!")
-        if water_level < 1:
-            raise ValueError(f"Error: Water level {water_level} is too low (min 1)")
-        elif water_level > 10:
-            raise ValueError(f"Error: Water level {water_level} is too high (max 10)")
-        else:
-            print(f"Water level {water_level} is good ")
-        if sunlight_hours < 2:
-            raise ValueError(f"Error: Sunlight hours {sunlight_hours} is too low (min 2)")
-        elif sunlight_hours > 12:
-            raise ValueError(f"Error: Sunlight hours {sunlight_hours} is too high (max 12)")
-        else:
-            print(f"Sunlight hours {sunlight_hours} is good")
-    except ValueError as e:
-        print(e)
+    if plant_name == "":
+        raise ValueError("Error: Plant name cannot be empty!")
+    elif plant_name == None:
+        raise ValueError("Error: Plant name cannot be None!")
+    if water_level < 1:
+        raise ValueError(f"Error: Water level {water_level} is too low (min 1)")
+    elif water_level > 10:
+        raise ValueError(f"Error: Water level {water_level} is too high (max 10)")
+    if sunlight_hours < 2:
+        raise ValueError(f"Error: Sunlight hours {sunlight_hours} is too low (min 2)")
+    elif sunlight_hours > 12:
+        raise ValueError(f"Error: Sunlight hours {sunlight_hours} is too high (max 12)")
+    print(f"Plant '{plant_name}' is healthy!")
 
 def test_plant_checks() -> None:
     print("=== Garden Plant Health Checker ===\n")
+    plant_name = "tomato"
+    water_level = 15
+    sunlight_hours = 0
+
     try:
         print("Testing good values...")
-        check_plant_health("tomato",5, 5)
-        print()
+        check_plant_health(plant_name, 5, 5)
     except Exception as e:
         print(e)
-
+    print()
     try:
         print("Testing empty plant name...")
         check_plant_health("",0, 0)
-        print()
+        print(f"Plant '{plant_name}' is healthy!")
     except Exception as e:
         print(e)
-
+    print()
     try:
         print("Testing bad water level...")
-        check_plant_health("tomato",15, 5)
-        print()
+        check_plant_health("tomato",water_level, sunlight_hours)
+        print(f"Water level {water_level} is good", end="\n\n")
     except Exception as e:
         print(e)
-    print("All error raising tests completed!")
-
+    print()
     try:
         print("Testing bad sunlight hours...")
-        check_plant_health("tomato",5, 0)
-        print()
+        check_plant_health("tomato",water_level, sunlight_hours)
+        print(f"Sunlight hours {sunlight_hours} is good")
     except Exception as e:
         print(e)
-
+    print()
     print("All error raising tests completed!")
 
 if __name__ == "__main__":
---
+++ ex5/ft_garden_management.py
@@ -0,0 +1,8 @@
+class GardenManager():
+    def __init__(self):
+        self.garden = {}
+
+def test_garden_management()
+
+if __name__ == "__main__":
+    test_garden_management()
